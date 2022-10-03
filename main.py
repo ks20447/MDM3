04f440c0a46b5e7aeaf50e34d19b9083c96c6571
Adam Morris <ks20447@bristol.ac.uk>
2022-10-03 19:57:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import simpy as sp
+import numpy as np
+import random as rn
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    env = sp.Environment()
+    x = rn.randint(1, 10)
+    print(x)
+    y = np.array([1, 2, 3])
+    print(y)
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
