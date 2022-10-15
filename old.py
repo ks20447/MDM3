25a77ef7e6cd3cbbf6e765c9cea7d8fd59a66083
wj19725 <60781744+wj19725@users.noreply.github.com>
2022-10-15 17:53:58 +0100

    Update old.py

--- old.py
+++ old.py
@@ -1,36 +1,92 @@
-# Important: Only push commits in the test branch to avoid breaking master file
-# Always git pull from master first, then switch to branch to commit
-import simpy as sp
+# This is a sample Python script.
 import random as rn
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
-TOTAL_BUYERS = 0        # Counter for the total buyers generated
-MAX_BUYERS = 20         # The maximum number of buyers that can exist at any time
-ALL_BUYERS = []         # List of buyer objects
-TOTAL_DEFAULTERS = 0    # Counter for number of defaulters
+ALL_BUYERS = []
+ALL_REJECTS = []
+ALL_LENDERS = []
+NUM_BUYERS = 0  # Simulation parameters and counters
+NUM_REJECTS = 0
+ALL_CUSTOMERS = 10
+NUM_LENDERS = 0
+MAX_LENDERS = 3
+num_accepted = 0
+num_rejected = 0
+
+
+class Customer:
+    def __init__(self, num, income, credit_band):
+
+        self.num = num
+        self.income = income
+        self.credit = credit_band  # Currently randomised
+        self.asset = rn.choice([15000, 30000, 60000])  # Currently randomised
+        self.allowance = (self.income * 0.3) / 12  # Allowance based off 50/30/20 rule of income spending
+        list = [True, False]
+        weights = [95, 5]
+        num_docs = 3
+        num_docs_approved = 0
+        global num_accepted, num_rejected
+        for i in (range(num_docs)):
+            possession_docs = rn.choices(list, weights, k=1)
+            if possession_docs == [True]:
+                num_docs_approved += 1
+            else:
+                break
+        if num_docs_approved == 3:
+            self.eligible = True
+            num_accepted += 1
+        else:
+            self.eligible = False
+            num_rejected += 1
 
 
 class Buyer:
-    def __init__(self, num, income, credit, documents, defaulting, status, asset, loan, duration, rate, monthly):
-        global TOTAL_BUYERS
-        TOTAL_BUYERS += 1
+    def __init__(self,num, income, credit_band, asset, allowance):
         self.num = num
         self.income = income
-        self.credit = credit
-        self.documents = documents
-        self.defaulting = defaulting
-        self.status = status
+        self.credit = credit_band
         self.asset = asset
-        self.loan = loan
-        self.duration = duration
-        self.rate = rate
-        self.monthly = monthly
+        self.allowance = allowance
+
+
+class Lender:
+    def __init__(self, num, credit_limit):
+        self.num = num
+        self.credit_limit = credit_limit
+        self.interest_rates = [9, 8, 7, 6, 5]
+        self.durations = [48, 36, 24, 12]
+
+    # Function for lender to check credit band against their specific credit threshold
+    def credit_check(self, buyer):
+        if buyer.credit < self.credit_limit:
+            print(f"Buyer {buyer.num} (Lender {self.num}): Rejected")
+            buyer.eligible = False
+
+    # Function for lender to generate offer to the buyer (currently maximises monthly payments/minimises duration)
+    def monthly_offer(self, buyer):
+        principle = buyer.asset
+        allowance = buyer.allowance
+        rate = self.interest_rates[buyer.credit]
+        r = rate / (12 * 100)
+        durations = self.durations
+        for i in range(len(durations)):
+            possible_payments = (principle * (r * ((1 + r) ** durations[i])) / (((1 + r) ** durations[i]) - 1))
+            if possible_payments > allowance and i > 0:
+                monthly_payments = (principle * (r * ((1 + r) ** durations[i - 1])) / (((1 + r) ** durations[i - 1]) - 1))
+                duration = durations[i - 1]
+                break
+            elif possible_payments > allowance and i == 0:
+                print(f"Buyer {buyer.num} (Lender {self.num}): Not enough income for asset loan")
+                monthly_payments = 0
+                duration = 0
+                buyer.eligible = False
+                break
+        return rate, monthly_payments, duration
 
 
 def income_generate():
-    # randomly chooses an income value based on ONS income distribution data in the UK
     income_table = pd.read_table('IncomeDistribution.txt')
     income_band = np.array(income_table[income_table.columns[0]])
     count = np.array(income_table[income_table.columns[1]])
@@ -40,92 +96,34 @@ def income_generate():
     income_data = np.concatenate(income_data)
     income = rn.choice(income_data)
     return income
-
-
-def credit_generate():
-    creditworthiness = np.random.randint(0, 100)    # to be swapped for credit score machine learning
-    defaulting = float(np.random.randint(0, 10)/1000)      # to be swapped for defaulting score machine learning
-    return creditworthiness, defaulting
-
-
-def documents_generate():
-    documents = 1111    # to be swapped with documentation algorithm
-    return documents
-
-
-def asset_generation(value):
-    asset = round(value*0.1, 1)     # to be swapped with asset generation procedure
-    return asset
-
-
-def credit_check(buyers_list):
-    global TOTAL_BUYERS
-    new_buyers = []
-    for i in range(len(buyers_list)):
-        temp_buyer = buyers_list[i]
-        buyer_num = temp_buyer.num
-        if temp_buyer.credit < 30:
-            print(f"Credit score is too low. Buyer {buyer_num} removed")
-            TOTAL_BUYERS -= 1
+# Press the green button in the gutter to run the script.
+def simulation_setup(num_customers, num_lenders):
+    global ALL_BUYERS, ALL_LENDERS, NUM_BUYERS, NUM_LENDERS, NUM_REJECTS
+    for i in range(num_customers):
+        customer_income = income_generate() * 1000
+        customer_band = rn.choice([0, 1, 2, 3, 4])  # This will be swapped for credit score process
+        if(Customer(i, customer_income, customer_band).eligible == True):
+            ALL_BUYERS.append(Customer(i, customer_income, customer_band))
+            NUM_BUYERS += 1
         else:
-            new_buyers.append(ALL_BUYERS[buyer_num])
-    if not new_buyers:
-        print(f"No buyers eligible")
-        TOTAL_BUYERS = 0
-    return new_buyers
+            ALL_REJECTS.append(Customer(i, customer_income, customer_band))
+            NUM_REJECTS += 1
+    for j in range(num_lenders):
+        lender_check = rn.choice([0, 1])
+        ALL_LENDERS.append(Lender(j, lender_check))
+        NUM_LENDERS += 1
 
 
-def setup_simulation(num_of_buyers):
-    for i in range(num_of_buyers):
-        buyer_income = income_generate()
-        [buyer_credit, buyer_defaulting] = credit_generate()
-        buyer_documents = documents_generate()
-        buyer_asset = asset_generation(buyer_income)
-        ALL_BUYERS.append(Buyer(i, buyer_income, buyer_credit, buyer_documents, buyer_defaulting, "Applying",
-                                buyer_asset, 0, 0, 0, 0))
-
-
-def loan_generate(asset_value):
-    if asset_value < 2:
-        loan = 15
-        loan_duration = 24
-        loan_rate = 0.09
-    elif 2 < asset_value < 4.0:
-        loan = 30
-        loan_duration = 36
-        loan_rate = 0.07
-    else:
-        loan = 60
-        loan_duration = 48
-        loan_rate = 0.05
-    monthly_payments = round((loan_rate / 12) * (1 / (1 - (1 + loan_rate / 12) ** (-loan_duration))) * loan, 3)
-    loan_total = monthly_payments * loan_duration
-    return loan_total, loan_duration, loan_rate, monthly_payments
-
-
-setup_simulation(MAX_BUYERS)
-print('Simulation Begins')
-ALL_BUYERS = credit_check(ALL_BUYERS)
-
-for i in range(len(ALL_BUYERS)):
-    current_buyer = ALL_BUYERS[i]
-    current_buyer.loan, current_buyer.duration, current_buyer.rate, current_buyer.monthly = \
-        loan_generate(current_buyer.asset)
-    print(f"Buyer {current_buyer.num} - Loan total: £{round(current_buyer.loan, 2)}k, monthly payments: "
-          f"£{round(current_buyer.monthly, 2)}k for {current_buyer.duration} months")
-    month_counter = 0
-    while current_buyer.loan > 0:
-        month_counter += 1
-        if rn.random() < current_buyer.defaulting:
-            print(f"Buyer {current_buyer.num} failed to pay on month: {month_counter}. Total loan remaining: "
-                  f"£{current_buyer.loan}k")
-            TOTAL_DEFAULTERS += 1
-            break
-        current_buyer.loan = round(current_buyer.loan - current_buyer.monthly)
-        current_buyer.duration -= 1
-    if current_buyer.loan == 0:
-        print(f"Buyer {current_buyer.num} successfully finished paying loan")
-print(f"Simulation finished - Number of Rejected Buyers: {MAX_BUYERS - TOTAL_BUYERS}, "
-      f"Number of Defaulters: {TOTAL_DEFAULTERS}")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+    simulation_setup(ALL_CUSTOMERS, MAX_LENDERS)
+    for current_buyer in ALL_BUYERS:
+        for current_lender in ALL_LENDERS:
+            current_lender.credit_check(current_buyer)
+            #if current_buyer.eligible:
+            offer_rate, offer_monthly, offer_duration = current_lender.monthly_offer(current_buyer)
+            #if current_buyer.eligible:
+            print(
+                f"Buyer {current_buyer.num} (Lender {current_lender.num}): Loan principle of £{current_buyer.asset} with an "
+                f"interest rate of {offer_rate}% for {offer_duration} months."
+                f" Total Monthly payments: £{offer_monthly:.2f}")
+    print(num_accepted, num_rejected)
