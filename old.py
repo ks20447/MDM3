7f2683e5665155e9b574a0a251b4ae83592027b2
wj19725 <60781744+wj19725@users.noreply.github.com>
2022-10-19 13:56:38 +0100

    Update old.py

--- old.py
+++ old.py
@@ -13,14 +13,25 @@ NUM_LENDERS = 0
 MAX_LENDERS = 3
 num_accepted = 0
 num_rejected = 0
-
+all_buyers_dict = []
+fields = ['num', 'income', 'house_owned', 'credit', 'married', 'gender', 'age', 'employed_time', 'fam_memb']
 
 class Customer:
     def __init__(self, num, income, credit_band):
 
         self.num = num
         self.income = income
+        self.house_owned = rn.randint(0, 1)  # 0 denotes no house, 1 means owns at least one house
         self.credit = credit_band  # Currently randomised
+        self.married = rn.randint(0, 1)  # 0 denotes not married, 1 means married
+        self.gender = rn.randint(0, 1)  # 0 denotes female, 1 means male
+        self.age = rn.randint(6570, 29200)  # minimum age is 18, maximum is 80
+        if self.age > 23725:
+            self.employed_time = rn.randint(0,
+                                            17155)  # minimum never worked, max worked his entire life since 18 up to age of retirement of 65
+        elif self.age <= 23725:
+            self.employed_time = rn.randint(0, self.age - 6570)
+        self.fam_memb = rn.randint(0, 6)
         self.asset = rn.choice([15000, 30000, 60000])  # Currently randomised
         self.allowance = (self.income * 0.3) / 12  # Allowance based off 50/30/20 rule of income spending
         list = [True, False]
@@ -42,13 +53,13 @@ class Customer:
             num_rejected += 1
 
 
-class Buyer:
-    def __init__(self,num, income, credit_band, asset, allowance):
-        self.num = num
-        self.income = income
-        self.credit = credit_band
-        self.asset = asset
-        self.allowance = allowance
+#class Buyer:
+    #def __init__(self,num, income, credit_band, asset, allowance):
+        #self.num = num
+        #self.income = income
+        #self.credit = credit_band
+        #self.asset = asset
+        #self.allowance = allowance
 
 
 class Lender:
@@ -98,12 +109,13 @@ def income_generate():
     return income
 # Press the green button in the gutter to run the script.
 def simulation_setup(num_customers, num_lenders):
-    global ALL_BUYERS, ALL_LENDERS, NUM_BUYERS, NUM_LENDERS, NUM_REJECTS
+    global ALL_BUYERS, ALL_LENDERS, NUM_BUYERS, NUM_LENDERS, NUM_REJECTS, fields, all_buyers_dict
     for i in range(num_customers):
         customer_income = income_generate() * 1000
         customer_band = rn.choice([0, 1, 2, 3, 4])  # This will be swapped for credit score process
         if(Customer(i, customer_income, customer_band).eligible == True):
             ALL_BUYERS.append(Customer(i, customer_income, customer_band))
+            all_buyers_dict.append(Customer(i, customer_income, customer_band).__dict__)
             NUM_BUYERS += 1
         else:
             ALL_REJECTS.append(Customer(i, customer_income, customer_band))
@@ -116,14 +128,15 @@ def simulation_setup(num_customers, num_lenders):
 
 if __name__ == '__main__':
     simulation_setup(ALL_CUSTOMERS, MAX_LENDERS)
+    df = pd.DataFrame(all_buyers_dict, columns = fields)
+    df.to_csv('https://github.com/ks20447/MDM3.git/buyers_dataframe.csv')
     for current_buyer in ALL_BUYERS:
         for current_lender in ALL_LENDERS:
             current_lender.credit_check(current_buyer)
             #if current_buyer.eligible:
             offer_rate, offer_monthly, offer_duration = current_lender.monthly_offer(current_buyer)
-            #if current_buyer.eligible:
-            print(
-                f"Buyer {current_buyer.num} (Lender {current_lender.num}): Loan principle of £{current_buyer.asset} with an "
-                f"interest rate of {offer_rate}% for {offer_duration} months."
-                f" Total Monthly payments: £{offer_monthly:.2f}")
-    print(num_accepted, num_rejected)
+            if current_buyer.eligible:
+                print(
+                    f"Buyer {current_buyer.num} (Lender {current_lender.num}): Loan principle of £{current_buyer.asset} with an "
+                    f"interest rate of {offer_rate}% for {offer_duration} months."
+                    f" Total Monthly payments: £{offer_monthly:.2f}")
