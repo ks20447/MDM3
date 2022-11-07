b10ca37922aeabd21f7882cf388a675f8e944e08
Adam <ks20447@bristol.ac.uk>
2022-11-07 14:12:39 +0000

    Final model 2 versions to produce results

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
-from matplotlib import pyplot as plt
 import random as rn
-import numpy as np
 from numpy.random import choice
 import time
 from colorama import Fore, Style
 from LogisticRegression import *
+from Results import *
 
 # Simulation parameters and counters
 ALL_BUYERS = []
 ALL_LENDERS = []
-TOTAL_CUSTOMERS = 10000
+TOTAL_CUSTOMERS = 20000
 NUM_BUYERS = 0
-MAX_BUYERS = 100
-NUM_LENDERS = 0
-MAX_LENDERS = 3
-ALL_BANDS = [0, 1, 2, 3, 4]
-ALL_ASSETS = [5000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
+ALL_ASSETS = np.array([5000, 6000, 7000, 8000, 9000, 10000, 12500, 15000, 17500, 20000])
+STANDARD_RATES = np.array([7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]) - 2
 SIMULATION_TIME = 120
-MARKET_VALUE = ALL_ASSETS[-1] * MAX_BUYERS * SIMULATION_TIME
+NUM_SIMS = 1
+MARKET_VALUE = ALL_ASSETS[-1] * SIMULATION_TIME * 100
 
 
 class Customer:
     def __init__(self, income):
         self.Client_Income = income
         self.House_Own = choice((0, 1), p=[0.398, 0.602])  # 0 denotes no house, 1 means owns at least one house
-        self.Credit_Amount = rn.choice(ALL_ASSETS)
+        self.Credit_Amount = find_nearest(ALL_ASSETS, income)
         self.Client_Marital_Status = choice(['M', 'W', 'S', 'D'], p=[0.5105, 0.0571, 0.3350, 0.0974])
         self.Client_Gender = choice(['Male', 'Female'], p=[0.50, 0.50])
         self.Age_Days = choice(choice([range(18, 20), range(20, 24), range(25, 29), range(30, 34),
@@ -37,13 +34,13 @@ class Customer:
             self.Employed_Days = 292
         elif 7300 < self.Age_Days <= 8760:
             self.Employed_Days = 475
-        elif 9125 <= self.Age_Days <= 12410:
+        elif 8760 < self.Age_Days <= 12410:
             self.Employed_Days = 1168
-        elif 12775 <= self.Age_Days <= 16060:
+        elif 12410 < self.Age_Days <= 16060:
             self.Employed_Days = 1935
-        elif 6425 <= self.Age_Days <= 19710:
+        elif 16060 < self.Age_Days <= 19710:
             self.Employed_Days = 2847
-        elif 20075 <= self.Age_Days <= 23360:
+        elif 19710 < self.Age_Days <= 23360:
             self.Employed_Days = 3760
         possession_docs = rn.choices([True, False], [95, 5], k=3)
         if all(possession_docs):
@@ -60,7 +57,7 @@ class Buyer:
         self.credit = credit_band                           # Currently randomised
         self.asset = asset
         self.offered_asset = 0
-        self.allowance = (self.income*0.3) / 12             # Allowance based off 50/30/20 rule of income spending
+        self.allowance = (self.income*0.3) / 12
         self.duration = rn.choice([12, 24, 36, 48])
         self.status = "Applying"
         self.eligible = True
@@ -81,10 +78,10 @@ class Buyer:
             if rn.random() < self.defaulting:
                 print(f"Buyer {self.num} (Lender {lender.num}) defaulted. "
                       f"Total remaining: £{round(total, 2)} ")
-                usage_parameter = 0.9
-                collateral = (self.asset - (counter - self.start_month) * usage_parameter) * 0.8
+                collateral = car_depreciation(self.asset, counter - self.start_month)
                 lender.loss += (total - collateral)
                 lender.collateral += collateral
+                lender.collateral_timeline[counter] = collateral
                 self.status = 'Defaulted'
             else:
                 lender.profit += monthly
@@ -92,17 +89,20 @@ class Buyer:
 
 # Lender object and attributes
 class Lender:
-    def __init__(self, num, credit_limit, max_duration, market_share):
+    def __init__(self, num, credit_limit, max_duration, market_share, rate_increase):
         self.num = num
         self.credit_limit = credit_limit
         self.max_duration = max_duration
         self.profit = 0
+        self.profit_timeline = []
         self.loss = 0
         self.collateral = 0
+        self.collateral_timeline = np.zeros(SIMULATION_TIME)
         self.max_loan = market_share
         self.current_loan = 0
         self.current_lending = 0
-        self.rates = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+        self.rates = STANDARD_RATES + rate_increase
+        self.colour = (rn.random(), rn.random(), rn.random())
 
     # Function for lender to check credit band against their specific credit threshold
     def credit_check(self, buyer):
@@ -220,14 +220,31 @@ def buyer_setup(index, t, buyers_dataframe):
         NUM_BUYERS += 1
 
 
-def lender_setup(market_share):
+def lender_setup(sim_num, market_share):
     global ALL_LENDERS
-    ALL_LENDERS.append(Lender(0, 3, 48, market_share * 0.5))
-    ALL_LENDERS.append(Lender(1, 10, 48, market_share * 0.5))
+    ALL_LENDERS.append(Lender(0, 5, 48, market_share * 0.5, sim_num))
 
 
 def ret_2nd_ele(tuple_1):
     return tuple_1[1]
+
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+
+def car_depreciation(asset, month_default):
+    if month_default < 13:
+        r = 25/12
+        collateral_val = (asset*(1-(r/100))**month_default)
+    else:
+        asset_at2 = (asset*(1-(25/1200))**11)
+        month_default -= 12
+        r = 15.6/12
+        collateral_val = (asset_at2*(1-(r/100))**month_default)
+    return collateral_val
 
 
 def generate_offers(all_buyers, all_lenders):
@@ -276,30 +293,41 @@ def loan_payments(month, all_buyers, all_lenders):
         print(f"Total amount borrowed by customers: £{lender.current_loan:,}")
         print(f"Current lending: £{round(lender.current_lending, 2):,}")
         print(f"")
+        lender.profit_timeline.append(lender.profit)
 
 
-def simulation(sim_time, num_customers, num_buyers):
+def simulation(sim_time, num_customers):
     global ALL_BUYERS, ALL_LENDERS
-    lender_setup(MARKET_VALUE)
-    index = 0
-    customer_setup(num_customers)
-    xd = predict_from_generated_customer("customers_dataframe.csv", "Train_Dataset.csv")
-    xd = xd.loc[xd["Band"] != 10]
-    xd.to_csv("buyers_dataframe.csv")
-    buyers_dataframe = pd.read_csv("buyers_dataframe.csv")
-    for t in range(sim_time):
-        print(Fore.BLUE + f"Month {t}" + Style.RESET_ALL)
-        if t % 6 == 0:
-            buyer_setup(index, t, buyers_dataframe[num_buyers*index:num_buyers+(num_buyers*index)])
-            current_buyers = ALL_BUYERS[num_buyers*index:num_buyers+(num_buyers*index)]
-            generate_offers(current_buyers, ALL_LENDERS)
-            index += 1
-            print(f"")
-        loan_payments(t, ALL_BUYERS, ALL_LENDERS)
+    profit_lending = np.zeros((NUM_SIMS, sim_time))
+    profit_absolute = np.zeros((NUM_SIMS, sim_time))
+    for i in range(NUM_SIMS):
+        ALL_BUYERS, ALL_LENDERS = [], []
+        lender_setup(i, MARKET_VALUE)
+        index = 0
+        customer_setup(num_customers)
+        xd = predict_from_generated_customer("customers_dataframe.csv", "Train_Dataset.csv")
+        xd = xd.loc[xd["Band"] != 10]
+        xd.to_csv("buyers_dataframe.csv")
+        buyers_dataframe = pd.read_csv("buyers_dataframe.csv")
+        num_buyers = int(len(buyers_dataframe)/sim_time)
+        for t in range(sim_time):
+            print(Fore.BLUE + f"Month {t} (Iteration {i})" + Style.RESET_ALL)
+            if t % 1 == 0:
+                buyer_setup(index, t, buyers_dataframe[num_buyers*index:num_buyers+(num_buyers*index)])
+                current_buyers = ALL_BUYERS[num_buyers*index:num_buyers+(num_buyers*index)]
+                generate_offers(current_buyers, ALL_LENDERS)
+                index += 1
+                print(f"")
+            loan_payments(t, ALL_BUYERS, ALL_LENDERS)
+        # plot_profit(ALL_LENDERS, SIMULATION_TIME)
+        # plot_collateral(ALL_LENDERS, SIMULATION_TIME)
+        profit_lending[i] = ALL_LENDERS[0].profit_timeline/ALL_LENDERS[0].current_loan
+        profit_absolute[i] = ALL_LENDERS[0].profit_timeline
+    plot_all_timelines(profit_lending, profit_absolute, SIMULATION_TIME)
 
 
 if __name__ == '__main__':
     start = time.perf_counter()
-    simulation(SIMULATION_TIME, TOTAL_CUSTOMERS, MAX_BUYERS)
+    simulation(SIMULATION_TIME, TOTAL_CUSTOMERS)
     end = time.perf_counter()
     print(f"Simulation finished in {round(end - start, 2)}s")
